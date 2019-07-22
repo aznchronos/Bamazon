3213f46fedc44d046398e392f7d202620f78dc2e
Andy Ren <andyr914@gmail.com>
2019-07-21 20:24:41 -0700

    Added initial display of products and added some prompts to determine items to display

--- bamazon.sql
+++ bamazon.sql
@@ -9,7 +9,23 @@ create table products (
     product_name varchar(100) null,
     department_name varchar(100) null,
     price float(14, 2) null,
-    stock_quantity int not null
+    stock_quantity int not null,
+    primary key (item_id)
 );
+
+insert into products (item_id, product_name, department_name, price, stock_quantity)
+values
+(1, "Potatoes 1lbs", "Grocery", 2.99, 99),
+(2, "Blinksys 5 Port 1GBPS Network Switch", "Computer", 37.85, 150),
+(3, "Bamazon Echo (2nd Generation", "Bamazon Devices & Accessories", 149.99, 125),
+(4, "BP-Link Modem", "Computer", 79.99, 75),
+(5, "Bead Bool Blu-Ray", "Movies & TV", 24.99, 333),
+(6, "Bamaha 76-Key Portable Keyboard", "Musical Instruments", 249.99, 50),
+(7, "Shrek 5: The Shrekening Blu-Ray", "Movies & TV", 19.99, 45),
+(8, "Carrots", "Grocery", 1.99, 80),
+(9, "Bamsung B10+ 256gb", "Cell Phones & Accessories", 750.00, 63),
+(10, "Bark Souls 3", "Video Games", 29.99, 13),
+(11, "Shrek 6: The Shrekinator Blu-Ray", "Movies & TV", 19.99, 17),
+(12, "Bastlevania: The Remake", "Video Games", 29.99, 69);
 
 select * from products;
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,16 +1,69 @@
-var inquirer = require("inquirer");
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
+    port: 3306,
     user: "root",
     password: "",
     database: "bamazon"
 });
 
-connection.connect(function(err){
-    if(err) throw err;
-    start();
-})
+start();
 
+function start(){
+    connection.query("Select * from products", function (err, res){
+        console.log("=======================================================\n");
+        for(var i = 0; i < res.length; i++){
+            if(res[i].item_id < 10){
+                console.log("||  " + res[i].item_id + " || " + res[i].product_name + "  $" + res[i].price);    
+            } else{
+                console.log("|| " + res[i].item_id + " || " + res[i].product_name + "  $" + res[i].price);
+            }
+        }
+        console.log("=======================================================\n");
+        
+    });
+    action();
+}
+
+function action(){
+    inquirer.prompt([
+        {
+            name: "action",
+            type: "list",
+            message: "Would you like to Purchase an item from our store?",
+            choices: ["Yes", "No"]
+        }
+    ]).then(function(res){
+        if(res.action == "Yes"){
+            inquirer.prompt([
+                {
+                    name: "id",
+                    type: "number",
+                    message: "Please input the ID of the item that you wish to purchase"
+                }
+            ]).then(function(res){
+                connection.query(
+                    "Select * from products where ?",
+                    {
+                        item_id: res.id
+                    },
+                    function(err,res){
+                        // if (err) throw err;
+                        // console.log("This was the response " + res);
+                        if (res === null){
+                            console.log("There was an error in this program");
+                            connection.end();
+                        } else {
+                            console.log(res[0].item_id);
+                        }
+                    }
+                )
+            })
+        } else if (res.action == "No"){
+            console.log("Thank you for your visit");
+            connection.end();
+        }
+    })
+}
